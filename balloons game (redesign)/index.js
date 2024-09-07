7b7b8c3d2bd4fd1dfff1bb53d927c96d94875613
CHIRAG RAVAL <131503978+chirag-raval-web@users.noreply.github.com>
2024-09-07 21:54:10 +0530

    created important variable

---
+++ balloons game (redesign)/index.js
@@ -0,0 +1,29 @@
+
+// common IDs
+let displayBalloon = document.getElementsByClassName('display-ballons');
+let startBtn = document.getElementById('start');
+let inputField = document.getElementById('input');
+let checkBtn = document.getElementById('check');
+let message = document.getElementById('message');
+
+//table IDs
+let score = document.getElementById('score');
+let stage = document.getElementById('stage');
+let winning = document.getElementById('winning');
+let lose = document.getElementById('lose');
+let tip = document.getElementById('tip');
+
+//counter IDs
+let counter = document.getElementById('counter');
+let startCountDown;
+let stopCountDown;
+let updateCountDown;
+let countDownSecond;
+
+//random number variable
+let randomNum;
+let randomImgSrc;
+
+// main functions
+let startGame = () =>{}
+let checkResult = () =>{}
