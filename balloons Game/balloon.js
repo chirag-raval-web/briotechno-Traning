5f087bca1dc674fa8ac8bf65a381564ca3c0bf36
chirag-raval-web <131503978+chirag-raval-web@users.noreply.github.com>
2024-09-07 13:39:55 +0530

    added stage , score, win ,lose functionality

--- balloons Game/balloon.js
+++ balloons Game/balloon.js
@@ -1,12 +1,38 @@
+//display balloons variable
+let displayBalloons = document.getElementById('display-ballons');
+
+//start,input,check,message ids
 let start = document.getElementById('start');
 let input = document.getElementById('inputCheck');
 let check = document.getElementById('check');
-let displayBalloons = document.getElementById('display-ballons');
 let msg = document.getElementById('message');
 
+//score,stage,winning,lose,timelesf table ids
+let score = document.getElementById('score');
+let stage = document.getElementById('stage');
+let winning = document.getElementById('winning');
+let lose = document.getElementById('lose');
+let timeLeft = document.getElementById('timeLeft');
+let tip = document.getElementById('tip');
 
+// console.log(score,stage,winning,lose,tip,timeLeft);
 
+//global variables..
+
+let stageCount = 1;
+let winCount = 0;
+let loseCount = 0;
+let timeLeftCount = 0;
 let randomNum;
+
+//assigning defult values
+score.innerText = 0;
+stage.innerText = stageCount;
+winning.innerText = winCount;
+lose.innerText = loseCount;
+timeLeft.innerText =timeLeftCount;
+
+
 
 // logic for add button 
 start.addEventListener('click', () => {
@@ -29,16 +55,14 @@ start.addEventListener('click', () => {
         item.classList.add(`ms-${Math.round(Math.random() * 5)}`);
         item.classList.add(`mt-${Math.round(Math.random() * 5)}`);
         item.classList.add(`mb-${Math.round(Math.random() * 5)}`);
+
         // add balloon in the div 
-        // item.innerText = `🎈`
         item.innerHTML = `<img src="./${totalBalloons[Math.round(Math.random() * 3)]}" class="balloon" alt="${totalBalloons[Math.round(Math.random() * 3)]}">`
 
-        // item.src = "./Balloon_Red.png";
+  
 
         displayBalloons.appendChild(item)
     }
-
-
 
 })
 
@@ -51,6 +75,15 @@ check.addEventListener('click', () => {
         msg.classList.add(alertMsg[1]);
         msg.innerText = "congratulation 🎉 You win"
         input.value = null;
+        winCount++;
+        winning.innerText = winCount;   
+        score.innerText = winCount*2;
+        displayBalloons.innerHTML='';
+        if (winCount == 5) {
+            stageCount++;
+            stage.innerText =stageCount;
+        } 
+
     } else if (input.value == '') {
 
         msg.classList.remove(alertMsg[0],alertMsg[1] ,alertMsg[2]);
@@ -62,5 +95,12 @@ check.addEventListener('click', () => {
         msg.classList.add(alertMsg[2]);
         msg.innerText = "WRONG ANSWER ! You Lose 😥"
         input.value = null;
+        loseCount++;
+        lose.innerText = loseCount;
+        displayBalloons.innerHTML='';
     }
 })
+
+let timeRemaining = () =>{
+    
+}
