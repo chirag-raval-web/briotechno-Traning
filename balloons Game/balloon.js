50b86aabf4c4276a3f4dc5b362f2ac92250b63c2
chirag-raval-web <131503978+chirag-raval-web@users.noreply.github.com>
2024-09-07 17:29:18 +0530

    some modification but occurs bugs

--- balloons Game/balloon.js
+++ balloons Game/balloon.js
@@ -40,42 +40,77 @@ winning.innerText = winCount;
 lose.innerText = loseCount;
 timeLeft.innerText = "00:00";
 
+let countdownTimer;
+let countdownSeconds = 60; 
+let countdownRunning = false;
 
+function updateCountdown() {
+    const minutes = Math.floor(countdownSeconds / 60);  
+    const seconds = countdownSeconds % 60;
+    timeLeft.textContent = 
+        `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
 
+function resetCountdown(newTime) {
+    if (countdownTimer) {
+        clearInterval(countdownTimer);  // Stop the current countdown
+        countdownRunning = false;       // Allow the countdown to start again
+    }
+    countdownSeconds = newTime;         // Reset the countdownSeconds to a new value (or default)
+    updateCountdown();                  // Update the display (optional)
+}
 
+// Function to start the countdown
+function startCountdown(stg) {
+    if (countdownRunning) return;
+    countdownRunning = true;
 
-start.addEventListener('click', () => { startGame() })
+    switch (+stg) {
+        case 1:
+            countdownTimer = setInterval(() => {
+                if (countdownSeconds > 0) {
+                    countdownSeconds--;
+                    updateCountdown();
+                } else {
+                    return countdownSeconds;
+                    clearInterval(countdownTimer);
+                    countdownRunning = false;
+                }
+            }, 1000);
+            break;
 
-check.addEventListener('click', () => { checkResult() })
+        case 2:
+            countdownTimer = setInterval(() => {
+                if (countdownSeconds > 5) {
+                    countdownSeconds--;
+                    updateCountdown();
+                } else {
+                    clearInterval(countdownTimer);
+                    countdownRunning = false;
+                }
+            }, 1000);
+            break;
 
+        default:
+            break;
+    }
+}
+ 
+start.onclick = () =>{startCountdown(stage.innerText)}
 
-
-// let timeRemaining = (stage) =>{
-
-//      timeLeftCount = 60;
-//     let intervalID = setInterval(() => {
-//     timeLeft.innerText =  timeLeftCount;
-//      if (timeLeftCount == 0) {
-
-//     clearInterval(intervalID); // Cancels the interval
-//   }
-//   hs--;
-
-// }, 1000);
-
-// }
-
+ 
+displayBalloons.innerHTML = '';
+ 
 
 //define all function 
 
 let startGame = () => {
-
-    displayBalloons.innerHTML = '';
+     startCountdown();
 
 
     // generate random number between 1 to 10 
-    randomNum = Math.round(Math.random() * 10);
-
+    randomNum = Math.round(Math.random() * (stageCount*10+1));
+    
     let totalBalloons = ['Balloon_Red.png', 'Balloon_Green.png', 'Balloon_Pink.png', 'Balloon_Blue.png'];
 
     // display random number of ballons
@@ -92,8 +127,17 @@ let startGame = () => {
         item.classList.add(`mb-${Math.round(Math.random() * 5)}`);
 
         // add balloon in the div 
-        item.innerHTML = `<img src="./${totalBalloons[Math.round(Math.random() * 3)]}" class="balloon" alt="${totalBalloons[Math.round(Math.random() * 3)]}">`
+        // item.innerHTML = `<img src="./${totalBalloons[Math.round(Math.random() * 3)]}"  class="balloon" alt="${totalBalloons[Math.round(Math.random() * 3)]}">`
 
+        if (stageCount => 1) {
+            
+        item.innerHTML = `<img src="./${totalBalloons[Math.round(Math.random() * 3)]}" height="50px"  class="balloon" alt="${totalBalloons[Math.round(Math.random() * 3)]}">`
+
+        
+        }else{
+        item.innerHTML = `<img src="./${totalBalloons[Math.round(Math.random() * 3)]}"  class="balloon" alt="${totalBalloons[Math.round(Math.random() * 3)]}">`
+
+        }
 
         displayBalloons.appendChild(item)
 
@@ -102,7 +146,7 @@ let startGame = () => {
 
 let checkResult = () => {
     alertMsg = ['d-none', 'alert-success', 'alert-danger', 'alert-warning'];
-
+    
     if (input.value == randomNum) {
         msg.classList.remove(alertMsg[0], alertMsg[2], alertMsg[3]);
         msg.classList.add(alertMsg[1]);
@@ -112,12 +156,13 @@ let checkResult = () => {
         winning.innerText = winCount;
         score.innerText = winCount * 2;
         displayBalloons.innerHTML = '';
-        if (winCount == 5) {
+        if (winCount%5 == 0 ) {
             stageCount++;
             stage.innerText = stageCount;
         }
-        showPopUp();
+        victory(stageCount);
         startGame();
+        alert(randomNum)
     } else if (input.value == '') {
 
         msg.classList.remove(alertMsg[0], alertMsg[1], alertMsg[2]);
@@ -138,23 +183,49 @@ let checkResult = () => {
     
 }
 
-
-let showPopUp = () =>{
+//model show function
+let victory = (stg) =>{
      
-    if (winning.innerText == 5 ||
-        winning.innerText == 10 ||
-        winning.innerText == 15 ||
-        winning.innerText == 20 ||
-        winning.innerText == 25 ||
-        winning.innerText == 30 ||
-        winning.innerText == 35 ||
-        winning.innerText == 40 ||
-        winning.innerText == 50 
-    ) {
+    if (stg*5 == 0 && stg !==0)   {
         modalOne.show();
+        winCount = 0;
     }
+   
+}
+let timesUp = () =>{
+
+if (winning.innerText !== 5  ||
+    winning.innerText !== 10 ||
+    winning.innerText !== 15 ||
+    winning.innerText !== 20 ||
+    winning.innerText !== 25 ||
+    winning.innerText !== 30 ||
+    winning.innerText !== 35 ||
+    winning.innerText !== 40 ||
+    winning.innerText !== 50 
+) {
+    modalSec.show();
+    
+}
 }
 
- 
+document.getElementById('playAgain').addEventListener('click',()=>{modelChoice(0)})
+document.getElementById('nextStage').addEventListener('click',()=>{modelChoice(1)})
+
+let modelChoice = (next) =>{
+    if (next==1) {
+        resetCountdown();
+        timeLeft.innerText = "00:00"
+    }else{
+        location.reload(true);
+    }
+    countdownRunning = false;
+    // startGame();
+}
+
+//start and check button function
+start.addEventListener('click', () => { startGame() })
+check.addEventListener('click', () => { checkResult() })
+
 //doc end
 });
