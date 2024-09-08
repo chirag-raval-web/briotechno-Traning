00fa409e9c1c879b4d4ee6e9f3e68fed4e3fa462
CHIRAG RAVAL <131503978+chirag-raval-web@users.noreply.github.com>
2024-09-09 04:56:16 +0530

    issue : bootstrap model not working

--- balloons game (redesign)/index.js
+++ balloons game (redesign)/index.js
@@ -11,6 +11,7 @@ let stage = document.getElementById("stage");
 let winning = document.getElementById("winning");
 let lose = document.getElementById("lose");
 let tip = document.getElementById("tip");
+tip.innerText = `Win 5 for next stage`;
 
 //currunt table data
 let currantWin = 0;
@@ -20,11 +21,18 @@ let currantStage = 1;
 
 //counter IDs
 let counter = document.getElementById("counter");
-let startCountDown;
 let stopCountDown;
-let updateCountDown;
-let countDownSecond;
-tip.innerText = `Win ${currantStage * 5} for next stage`;
+// let updateCountDown;
+let countDownSecond = 30;
+let countdownRunning = false;
+// let startCountDown;
+ 
+
+//model variables
+// const victoryModelElm = document.getElementById('victoryModel');
+// const modalOne = new bootstrap.Modal(victoryModelElm);
+// const timeUpModelElm = document.getElementById('timeOutModel');
+// const modalSec = new bootstrap.Modal(timeUpModelElm);
 //random number variable
 let randomNum;
 
@@ -40,9 +48,13 @@ let randomImgSrc = () => {
 
 // main functions
 let startGame = () => {
-  let stgBallon = currantStage * 10;
-  randomNum = Math.round(Math.random() * stgBallon) + 1;
+  startCountDown();
+  stageCheck();
 
+
+  // let stgBallon = currantStage * 10;
+  randomNum = Math.round(Math.random() * 10) + 1;
+ 
   displayBalloon.innerHTML = "";
 
   for (let i = 1; i <= randomNum; i++) {
@@ -63,6 +75,9 @@ let startGame = () => {
   }
 };
 let checkResult = () => {
+
+  
+
   message.classList.remove("alert-success");
   message.classList.remove("alert-warning");
   message.classList.remove("alert-danger");
@@ -92,20 +107,80 @@ let checkResult = () => {
     score.innerText = currantScore;
     startGame();
   }
+
 };
 
 let stageCheck = () => {
-  if (currantStage * 5 - currantWin === 0) {
-    tip.innerText = `Win ${currantStage * 5} for next stage`;
+  countdownRunning = false;
+  if (+stage.innerText * 5 - +winning.innerText === 0) {
+    tip.innerText = `Win ${+stage.innerText * 5} for next stage`;
   } else {
     tip.innerText = `Win ${currantStage * 5 - currantWin} for next stage`;
   }
   if (currantWin == currantStage * 5) {
+    dialogBox(1);
     currantStage++;
     currantWin = 0;
     stage.innerText = currantStage;
   }
 };
+function dialogBox(state) {
+  if (stage = 1) {
+    let cmf =confirm('congratulation 🎉,You win this stage \nclick ok start next round' );
+    if (cmf===true) {
+      startGame();
+      
+      
+    }else{
+      window.Navigator.reload();
+    }
+  }
+}
+function startCountDown() {
+  switch (currantStage) {
+    case 1:
+      countDownSecond = 30;
+      break;
+    case 2:
+      countDownSecond = 35;
+      break;
+    case 3:
+      countDownSecond = 40;
+      break;
+    case 4:
+      countDownSecond = 45;
+      break;
+    case 5:
+      countDownSecond = 50;
+      break;
+    default:
+      countDownSecond = 30;
+      break;
+  }
+  if (countdownRunning) return;
+  countdownRunning = true;
+  setInterval(() => {
+    if (countDownSecond > 0) {
+      countDownSecond--;
+      updateCountDown()
+    }else{
+      clearInterval();
+      alert("times up");
+      if (alert("times up")) {
+        countdownRunning = false;
+      }
+    }
+  }, 1000);
+}
+ 
+
+function updateCountDown(){
+ 
+ const minutes = Math.floor(countDownSecond / 60);  
+ const seconds = countDownSecond % 60;
+ counter.textContent = 
+     `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
 
 startBtn.addEventListener("click", () => startGame());
 checkBtn.addEventListener("click", () => checkResult());
